a6fa44dfec525fab96f2b40aaf1815791ec1afa3
bryanjoker <bryanjoker2@gmail.com>
2025-11-03 12:50:07 -0300

    template 03/11

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMubT2B3lqORc5lfNa6EhNELY_1ftjgLU",
+  authDomain: "frontendweb-2db6b.firebaseapp.com",
+  projectId: "frontendweb-2db6b",
+  storageBucket: "frontendweb-2db6b.firebasestorage.app",
+  messagingSenderId: "278482012621",
+  appId: "1:278482012621:web:37b239e9cf2567d121dc84"
+};
